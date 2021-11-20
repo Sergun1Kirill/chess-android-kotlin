7c260f948e44cb234059a8be1d536a3540806afc
Sergun Kirill <kirillsergun@gmail.com>
2021-11-20 17:45:49 +0300

    Drawing a white queen

--- chess/app/src/main/java/com/sergun/chess/ChessView.kt
+++ chess/app/src/main/java/com/sergun/chess/ChessView.kt
@@ -2,25 +2,21 @@ package com.sergun.chess
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
-//Отрисовка доски
+
 class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     private final val originX = 20f
     private final val originY = 200f
     private final val cellSide = 130f
 
-
+    //Отрисовка доски
     override fun onDraw(canvas: Canvas?) {
         val paint = Paint()
         for(i in 0..7) {
             for (j in 0..7) {
-
-
                 paint.color = if((i + j) % 2 == 1) Color.DKGRAY else Color.LTGRAY
                 canvas?.drawRect(
                     originX + i * cellSide,
@@ -29,7 +25,11 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
                     originY +  (j + 1) * cellSide, paint
                 )
             }
-
         }
+
+        // Рисование фигуры 
+        val whiteQueenBitmap = BitmapFactory.decodeResource(resources, R.drawable.queen_white)
+        canvas?.drawBitmap(whiteQueenBitmap, null, Rect(0, 0, 600, 600), paint)
+
     }
 }
