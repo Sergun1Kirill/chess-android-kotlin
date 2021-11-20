97b1c35f943294838aaf8cdaff5f7e2882875dd4
Sergun Kirill <kirillsergun@gmail.com>
2021-11-20 18:20:01 +0300

    IMG bitmap

--- chess/app/src/main/java/com/sergun/chess/ChessView.kt
+++ chess/app/src/main/java/com/sergun/chess/ChessView.kt
@@ -12,9 +12,42 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
     private final val originY = 200f
     private final val cellSide = 130f
 
-    //Отрисовка доски
+    private final val imgResIds = setOf(
+        R.drawable.bishop_black,
+        R.drawable.bishop_white,
+        R.drawable.king_black,
+        R.drawable.king_white,
+        R.drawable.queen_black,
+        R.drawable.queen_white,
+        R.drawable.rook_black,
+        R.drawable.rook_white,
+        R.drawable.knight_black,
+        R.drawable.knight_white,
+        R.drawable.pawn_black,
+        R.drawable.pawn_white,
+    )
+    private final val bitMaps = mutableMapOf<Int, Bitmap>()
+    private final val paint = Paint()
+
+    init {
+        loadBitmaps()
+    }
+    // Рисование фигуры
     override fun onDraw(canvas: Canvas?) {
-        val paint = Paint()
+        drawChessBoard(canvas)
+
+        val whiteQueenBitmap = bitMaps[R.drawable.queen_black]!!
+        canvas?.drawBitmap(whiteQueenBitmap, null, Rect(0, 0, 600, 600), paint)
+
+    }
+    private fun loadBitmaps() {
+      imgResIds.forEach {
+          bitMaps[it] = BitmapFactory.decodeResource(resources, it)
+      }
+    }
+
+    //Отрисовка доски
+    private fun drawChessBoard(canvas: Canvas?) {
         for(i in 0..7) {
             for (j in 0..7) {
                 paint.color = if((i + j) % 2 == 1) Color.DKGRAY else Color.LTGRAY
@@ -26,10 +59,6 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
                 )
             }
         }
-
-        // Рисование фигуры 
-        val whiteQueenBitmap = BitmapFactory.decodeResource(resources, R.drawable.queen_white)
-        canvas?.drawBitmap(whiteQueenBitmap, null, Rect(0, 0, 600, 600), paint)
 
     }
 }
